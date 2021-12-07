b0b79774a51372199b73f0d1dc18fcb6ca897fc8
Geovanne Vichiatto <geovanne.vichiatto@tcsindustrial.com.br>
2021-12-07 11:22:43 -0300

    Adicionando validações no formulário.

--- front-end/src/views/Alunos.vue
+++ front-end/src/views/Alunos.vue
@@ -4,43 +4,51 @@
 			<b-container fluid>
 				<div class="table-title">
 					<h2>Listagem de Alunos</h2>
-					<b-button class="add-aluno-btn" v-b-modal.add-modal @click="resetForm()">Adicionar</b-button>
+					<b-button class="add-aluno-btn" v-b-modal.form-modal @click="resetForm()">Adicionar</b-button>
 				</div>
 				<div>
-							<table class="table table-hover align-middle">
-								<thead>
-									<tr>
-										<th>Nome</th>
-										<th>Endereço</th>
-										<th>Foto</th>
-										<th>Ação</th>
-									</tr>
-								</thead>
-								<tbody v-if="!tableIsLoading">
-									<tr v-for="i in items" :key="`tableRow${i.id}`">
-										<td>{{ i.nome }}</td>
-										<td>{{ i.endereco }}</td>
-										<td style="max-width: 50px"><b-img v-bind="mainProps" :src="i.foto"></b-img></td>
-										<td class="clicable">
-											<b-icon-pencil class="icon" v-b-modal.add-modal title="Editar" @click="editModal(i)"></b-icon-pencil>
-											<b-icon-trash class="icon" title="Deletar" @click="deleteAluno(i.id)"></b-icon-trash>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-							<div v-if="tableIsLoading" class="loading">
-								<b-spinner class="align-middle"></b-spinner>
-								<strong>Carregando...</strong>
-							</div>
+					<table class="table table-hover align-middle">
+						<thead>
+							<tr>
+								<th>Nome</th>
+								<th>Endereço</th>
+								<th>Foto</th>
+								<th>Ação</th>
+							</tr>
+						</thead>
+						<tbody v-if="!tableIsLoading">
+							<tr v-for="i in items" :key="`tableRow${i.id}`">
+								<td>{{ i.nome }}</td>
+								<td>{{ i.endereco }}</td>
+								<td style="max-width: 50px"><b-img v-bind="mainProps" :src="i.foto"></b-img></td>
+								<td class="clicable">
+									<b-icon-pencil class="icon" v-b-modal.form-modal title="Editar" @click="editModal(i)"></b-icon-pencil>
+									<b-icon-trash class="icon" title="Deletar" @click="deleteAluno(i.id)"></b-icon-trash>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<div v-if="tableIsLoading" class="loading">
+						<b-spinner class="align-middle"></b-spinner>
+						<strong>Carregando...</strong>
+					</div>
 				</div>
 			</b-container>
 		</div>
-		<b-modal id="add-modal" @ok="submitForm(form)" centered :title="modalTitle" @show="onModalLoaded()" @hidden="onModalHidden()">
+		<b-modal 
+		id="form-modal" 
+		@ok="submitForm" 
+		centered 
+		:title="modalTitle" 
+		@show="onModalLoaded()" 
+		@hidden="onModalHidden()"
+		cancel-title="Cancelar"
+		>
 			<b-form>
 				<b-form-group
-					id="input-group-1"
-					label="Nome do(a) Aluno(a):"
-					label-for="input-1"
+				id="input-group-1"
+				label="Nome do(a) Aluno(a):"
+				label-for="input-1"
 				>
 					<b-form-input
 					id="input-1"
@@ -48,7 +56,11 @@
 					type="email"
 					placeholder="Nome Completo"
 					required
+					:state="formValidation.nome.state"
 					></b-form-input>
+					<b-form-invalid-feedback :state="formValidation.nome.isOk && formValidation.nome.isTypeOk">
+						É necessário digitar um nome válido
+					</b-form-invalid-feedback>
 				</b-form-group>
 
 				<b-form-group id="input-group-2" label="Endereço:" label-for="input-2">
@@ -57,7 +69,11 @@
 					v-model="form.endereco"
 					placeholder="Endereço"
 					required
+					:state="formValidation.endereco.state"
 					></b-form-input>
+					<b-form-invalid-feedback :state="formValidation.endereco.isOk && formValidation.endereco.isTypeOk">
+						É necessário digitar um endereço válido
+					</b-form-invalid-feedback>
 				</b-form-group>
 
 				<b-form-group id="input-group3" label="Foto" laber-for="input-3" v-if="loaded">
@@ -79,8 +95,11 @@
 						drag: 'Escolha um arquivo ou arrastre sua imagem pra cá'
 					}">
 					</picture-input>
+					<div class="image-fail" v-if="!formValidation.foto.state">
+						{{ imageFailText }}
+						<img src="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none' stroke='%23dc3545' viewBox='0 0 12 12'><circle cx='6' cy='6' r='4.5'/><path stroke-linejoin='round' d='M5.8 3.6h.4L6 6.5z'/><circle cx='6' cy='8.2' r='.6' fill='%23dc3545' stroke='none'/></svg>">
+					</div>
 				</b-form-group>
-				
 			</b-form>
 		</b-modal>
 	</div>
@@ -97,7 +116,7 @@ export default ({
 			items: [],
 			form: {
 				nome: '',
-				edereco: '',
+				endereco: '',
 				foto: [],
 			},
 			show: true,
@@ -112,6 +131,25 @@ export default ({
 			modalTitle: '',
 			loaded: false,
 			tableIsLoading: false,
+			formValidation: {
+				nome: {
+					isOk: true,
+					isTypeOk: true,
+					state: null
+				},
+				endereco: {
+					isOk: true,
+					isTypeOk: true,
+					state: null
+				},
+				foto: {
+					isOk: true,
+					isTypeOk: true,
+					isFormatOk: true,
+					state: true
+				},
+			},
+			imageFailText: "",
 		}
 
 	},
@@ -145,6 +183,7 @@ export default ({
 		},
 
 		editModal(element) {
+			this.validationReset();
 			this.form = JSON.parse(JSON.stringify(element));
 			this.form.id = element.id;
 			this.url = JSON.parse(JSON.stringify(this.form.foto));
@@ -152,38 +191,45 @@ export default ({
 			this.modalTitle = 'Editar registro de aluno';
 		},
 
-		async submitForm(form) {
-			form.foto = this.imageBase64URL;
-			if (this.action == 'add') {
-				await this.postAluno(form).then((result) => {
-					if (result) {
-						this.$swal.fire({
-							title: 'Aluno(a) adicionado(a)!',
-							text: 'Registro de aluno adicionado.',
-							icon: 'success',
-							confirmButtonColor: 'rgba(84, 19, 153, 0.8)',
-						}).then((result) => {
-							if (result) {
-								this.getAlunos();
-							}
-						})
-					}
-				})
-			} else if (this.action == 'edit') {
-				await this.updateAluno(form).then((result) => {
-					if (result) {
-						this.$swal.fire({
-							title: 'Atualizado!',
-							text: 'Registro de aluno atualizado.',
-							icon: 'success',
-							confirmButtonColor: 'rgba(84, 19, 153, 0.8)',
-						}).then((result) => {
-							if (result) {
-								this.getAlunos();
-							}
-						})
-					}
-				})
+		async submitForm(bvModalEvt) {
+			bvModalEvt.preventDefault()
+			this.form.foto = this.imageBase64URL ? this.imageBase64URL : this.url;
+			if (this.validateForm(this.form)) {
+				this.$nextTick(() => {
+          			this.$bvModal.hide('form-modal')
+        		})
+				this.validationReset();
+				if (this.action == 'add') {
+					await this.postAluno(this.form).then((result) => {
+						if (result) {
+							this.$swal.fire({
+								title: 'Aluno(a) adicionado(a)!',
+								text: 'Registro de aluno adicionado.',
+								icon: 'success',
+								confirmButtonColor: 'rgba(84, 19, 153, 0.8)',
+							}).then((result) => {
+								if (result) {
+									this.getAlunos();
+								}
+							})
+						}
+					})
+				} else if (this.action == 'edit') {
+					await this.updateAluno(this.form).then((result) => {
+						if (result) {
+							this.$swal.fire({
+								title: 'Atualizado!',
+								text: 'Registro de aluno atualizado.',
+								icon: 'success',
+								confirmButtonColor: 'rgba(84, 19, 153, 0.8)',
+							}).then((result) => {
+								if (result) {
+									this.getAlunos();
+								}
+							})
+						}
+					})
+				} else return;
 			} else return;
 		},
 
@@ -193,10 +239,12 @@ export default ({
 				endereco: '',
 				foto: ''
 			},
-			this.url = '',
-			this.image = [],
-			this.action = 'add',
+			this.url = ''
+			this.image = []
+			this.action = 'add'
 			this.modalTitle = 'Adicionar registro de aluno'
+			this.validationReset()
+			this.imageBase64URL = ''
 		},
 
 		deleteAluno(id) {
@@ -247,6 +295,76 @@ export default ({
 
 		async updateAluno(aluno) {
 			return await AlunosRestResource.updateAluno(aluno);
+		},
+
+		validateForm(form) {
+			this.validationReset();
+			var validationFailed = 0;
+			if (form.nome) {
+				if (typeof form.nome !== 'string') {
+					this.formValidation.nome.isTypeOk = false
+					this.formValidation.nome.state = false
+					validationFailed++;
+				}
+			} else {
+				this.formValidation.nome.isOk = false
+				this.formValidation.nome.state = false
+				validationFailed++;
+			}
+
+			if (form.endereco) {
+				if (typeof form.endereco !== 'string') {
+					this.formValidation.endereco.isTypeOk = false
+					this.formValidation.endereco.state = false
+					validationFailed++;
+				}
+			} else {
+				this.formValidation.endereco.isOk = false
+				this.formValidation.endereco.state = false
+				validationFailed++;
+			}
+
+			if (form.foto) {
+				if (typeof form.foto !== 'string') {
+					this.imageFailText = "Arquivo inválido!"
+					this.formValidation.foto.isTypeOk = false
+					this.formValidation.foto.state = false
+					validationFailed++;
+				} else if (!form.foto.includes('data:image/jpeg;base64,')) {
+					this.imageFailText = "A imagem escolhida não está no formato *.jpg"
+					this.formValidation.foto.isFormatOk = false
+					this.formValidation.foto.state = false
+					validationFailed++;
+				}
+			} else {
+				this.imageFailText = "É necessário escolher uma foto"
+				this.formValidation.foto.isOk = false
+				this.formValidation.foto.state = false
+				validationFailed++;
+			}
+			return validationFailed ? false : true
+		},
+
+		validationReset() {
+			this.formValidation = {
+				nome: {
+					isOk: true,
+					isTypeOk: true,
+					state: null
+				},
+				endereco: {
+					isOk: true,
+					isTypeOk: true,
+					state: null
+				},
+				foto: {
+					isOk: true,
+					isTypeOk: true,
+					isFormatOk: true,
+					state: true
+				},
+			};
+			this.imageFailText = "";
 		}
 	}
 })
@@ -288,7 +406,25 @@ export default ({
 }
 
 .loading {
-	color: rgba(153, 19, 21, 0.75);
+	color: rgba(84, 19, 153, 0.7);
 }
 
+.image-fail {
+	width: 100%; 
+	margin-top: 0.25rem; 
+	font-size: 80%; 
+	color: #dc3545;
+	display: flex;
+    justify-content: space-between;
+}
+
+.btn-primary {
+	background-color: #589913 !important;
+	border-color: #589913 !important;
+}
+
+.btn-primary:hover {
+	background-color: #6bb819 !important;
+	border-color: #6bb819 !important;
+}
 </style>
