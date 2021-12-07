beaa0fd2793d1d6e0ad2ee2a1ea15f3797ebca52
Geovanne Vichiatto <geovanne.vichiatto@tcsindustrial.com.br>
2021-12-07 00:24:17 -0300

    Refatorações back-end

--- back-end/repository/alunos.js
+++ back-end/repository/alunos.js
@@ -1,4 +1,5 @@
 const models = require('../database/models/index');
+const imageDataURI = require('image-data-uri');
 
 module.exports = {
     findAllAlunos() {
@@ -9,8 +10,13 @@ module.exports = {
         });
     },
 
-    createAluno(data) {
-        return models.Alunos.create(data);
+    async createAluno(data) {
+        var img = await imageDataURI.decode(data.foto);
+        return models.Alunos.create({
+            nome: data.nome,
+            endereco: data.endereco,
+            foto: img.dataBuffer
+        });
     },
 
     deleteByID(id) {
