7d9e41bf2b74e29de1bfc7de58d741d9f65828bc
Geovanne Vichiatto <geovanne.vichiatto@tcsindustrial.com.br>
2021-12-06 21:12:50 -0300

    front.

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import store from '../store';
 
-const apiRestHost = "https://3.144.144.228:3000";
+const apiRestHost = "3.144.144.228:3000";
 
 const client = axios.create({
 	baseURL: `//${apiRestHost}/`,
